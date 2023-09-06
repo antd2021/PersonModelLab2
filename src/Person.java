edd90462d9f22d86e037dacc7381992656858f02
antd2021 <dininnam@mail.uc.edu>
2023-09-05 21:28:15 -0400

    Original commit failed

--- src/Person.java
+++ src/Person.java
@@ -5,4 +5,54 @@ public class Person {
     private String title;
     private int dateOfBirth;
 
+    //Constructors
+
+    public Person(String id, String firstName, String lastName, String title, int dateOfBirth) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.title = title;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(int dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
